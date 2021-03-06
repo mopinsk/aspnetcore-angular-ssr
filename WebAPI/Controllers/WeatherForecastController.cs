45ef8eeb99cf28ed7651d13fccdaf0834da5ca62
mopinsk <29751071+mopinsk@users.noreply.github.com>
2021-03-06 11:55:38 +0100

    add ASP.NET Core Web API

---
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            var forecasts = Enumerable.Range(1, 5)
+                .Select(index => new WeatherForecast(DateTime.Now.AddDays(index), rng.Next(-20, 55), Summaries[rng.Next(Summaries.Length)]))
+            .ToArray();
+
+            _logger.LogDebug("Created forecasts {Forecasts}", forecasts);
+
+            return forecasts;
+        }
+    }
+}
